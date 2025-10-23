6f5c1648049b7a41e883122d2ffeb84b773f8edf
ohchanju <ohchanju3@naver.com>
2025-10-23 14:38:41 +0900

    [FE] style: nav바 타입 정의 #1

---
+++ FE/src/widgets/Nav/types.ts
@@ -0,0 +1,13 @@
+export type NavVariant =
+  | "auth" // 로그인/회원가입
+  | "folder" // 폴더
+  | "pre" // 수업 전
+  | "live" // 수업 중
+  | "live-recording" // 수업 중 - 녹음 중
+  | "post" // 수업 후
+  | "exam"; // 시험
+
+export type NavMeta = {
+  variant: NavVariant;
+  title?: string | ((params: Record<string, string>) => string);
+};
