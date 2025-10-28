4fec52f412827f1dc826b827c37c07802a86da2e
ohchanju <ohchanju3@naver.com>
2025-10-28 14:46:47 +0900

    [FE] api: 로그인 api 연결 #22

--- FE/src/pages/auth/Login.tsx
+++ FE/src/pages/auth/Login.tsx
@@ -2,10 +2,26 @@ import { useContrastImage } from "@shared/useContrastImage";
 import * as s from "./Login_style";
 import InputField from "@layouts/components/login/InputContainer";
 import { useNavigate } from "react-router-dom";
+import { loginApi } from "@apis/auth/login";
+import { useState } from "react";
 
 const Login = () => {
   const logo = useContrastImage("/img/login/logoBig");
   const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await loginApi({ username, password });
+    if (res) {
+      localStorage.setItem("access", res.access);
+      localStorage.setItem("refresh", res.refresh);
+      navigate("/");
+    } else {
+      alert("아이디나 비밀번호를 확인하세요.");
+    }
+  };
 
   const handleSignupClick = () => {
     navigate("/signup");
@@ -21,9 +37,20 @@ const Login = () => {
       <s.LoginRightContaienr>
         <h1>캠퍼스 메이트에 오신 것을 환영합니다.</h1>
         <s.InputContainer>
-          <InputField label="아이디" placeholder="아이디를 입력하세요" />
-          <InputField label="비밀번호" placeholder="비밀번호 입력하세요" />
-          <button>로그인</button>
+          <InputField
+            label="아이디"
+            placeholder="아이디를 입력하세요"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <InputField
+            label="비밀번호"
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>로그인</button>
           <s.SignupContainer>
             <p>캠퍼스 메이트가 처음이신가요?</p>
             <p className="signup" onClick={handleSignupClick}>
