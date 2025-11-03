a3c6a53c8c867c16444620d80f1b7446675d09f5
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2025-11-04 05:12:37 +0900

    [BE] Feat: 교수 발화 STT 요약문 생성 #29

--- BE/lecture_docs/urls.py
+++ BE/lecture_docs/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('doc/<int:docId>/', DocDetailView.as_view(), name='doc-detail'),
     path('doc/<int:docId>/<int:pageNumber>/', PageDetailView.as_view(), name='page-detail'),
     path('page/<int:pageId>/board/', BoardView.as_view(), name='board-upload'),
-    path('board/<int:boardId>/', BoardView.as_view(), name="board-detail")
+    path('board/<int:boardId>/', BoardView.as_view(), name="board-detail"),
+    path("doc/<int:docId>/speech/summary/", DocSttSummaryView.as_view(), name="doc-stt-summary"),
 ]
