1cc07f5aacc1ef8e547245eb15f0e565f1b40ce2
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2025-11-05 00:17:09 +0900

    [BE] Feat: 사용자 설정 목소리, 속도 반영하여 TTS 생성 #28

--- BE/classes/views.py
+++ BE/classes/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from classes.serializers import SpeechCreateSerializer
 from classes.models import Speech
-from classes.utils import speech_to_text, text_to_speech
+from classes.utils import speech_to_text, text_to_speech, text_to_speech_local
 from lecture_docs.models import Page
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
 import traceback
+from rest_framework.views import APIView
 
 """
 1. 음성 파일(STT 변환)
@@ -15,8 +16,11 @@ import traceback
 4. speechId, stt, tts, page 반환
 """
 class SpeechView(generics.CreateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def post(self, request, pageId):
         try:
+            user = request.user
             # ✅ 1️⃣ pageId로 Page 객체 조회
             page = Page.objects.get(id=pageId)
         except Page.DoesNotExist:
@@ -34,8 +38,11 @@ class SpeechView(generics.CreateAPIView):
             if not stt_text or stt_text.strip() == "":
                 return JsonResponse({"error": "변환된 텍스트가 비어 있습니다."}, status=400)
             
+            voice = (user.voice or "여성")
+            rate = (user.rate or "보통")
+
             # 2️⃣ TTS 변환 + S3 업로드
-            s3_url = text_to_speech(stt_text, s3_folder="tts/")
+            s3_url = text_to_speech(stt_text, voice, rate, s3_folder="tts/")
 
             # 3️⃣ DB 저장
             speech = Speech.objects.create(
@@ -56,5 +63,31 @@ class SpeechView(generics.CreateAPIView):
             traceback.print_exc()  # 서버 로그 출력용
             return JsonResponse(
                 {"error": f"TTS 변환 중 오류가 발생했습니다: {str(e)}"},
+                status=500
+            )
+        
+class TTSTestView(APIView):
+    """
+    TTS 변환 로컬 저장 테스트용 API
+    """
+    def post(self, request):
+        try:
+            test_text = request.data["text"]
+            voice = request.data["voice"]
+            rate = request.data["rate"]
+
+            # TTS 변환 (로컬에만 저장)
+            local_path = text_to_speech_local(test_text, voice, rate)
+
+            return JsonResponse({
+                "message": "TTS 변환 성공",
+                "test_text": test_text,
+                "local_path": local_path
+            }, status=200)
+
+        except Exception as e:
+            traceback.print_exc()
+            return JsonResponse(
+                {"error": f"TTS 테스트 중 오류 발생: {str(e)}"},
                 status=500
             )
