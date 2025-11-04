196da40d527c451eaa0e8d173c33d12c56478742
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2025-11-05 00:17:33 +0900

    [BE] Feat: 로컬 TTS 파일 생성 API #28

--- BE/classes/urls.py
+++ BE/classes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from classes.views import SpeechView
+from classes.views import SpeechView, TTSTestView
 
 urlpatterns = [
     path('speech/<int:pageId>/', SpeechView.as_view(), name='speech_create'),
+    path('tts/test/', TTSTestView.as_view(), name='tts_test')
 ]
